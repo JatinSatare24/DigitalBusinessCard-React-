02fc4c9ea25039146a861e17aacf01bd8c576523
Jatin Satare <jatinsatare@gmail.com>
2026-02-06 13:59:05 +0530

    add Footer component

---
+++ DigitalBusinessCard/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export function Footer() {
+    return (
+        <footer>
+            <div>
+                <img src="src/images/Facebookicon.png" alt="fb icon" />
+                <img src="src/images/GitHubIcon.png" alt="Github icon" />
+                <img src="src/images/InstagramIcon.png" alt="IG icon" />
+                <img src="src/images/TwitterIcon.png" alt="Twiiter icon" />
+            </div>
+        </footer>
+    )
+}
